61e45d67b93e8973b13beab6602a682d0bc96f9e
Andrew Eland <andrew@andreweland.org>
2017-03-26 21:35:34 +0100

    Download vector_tile.proto in setup.sh instead of bundling

--- setup.sh
+++ setup.sh
@@ -4,5 +4,8 @@ go get github.com/golang/protobuf/proto
 go get github.com/golang/geo/s2
 go get github.com/paulmach/go.geo
 go get github.com/gorilla/handlers
+test -d proto || mkdir proto
+curl -o proto/vector_tile.proto https://raw.githubusercontent.com/mapbox/vector-tile-spec/master/2.1/vector_tile.proto
+test -d data || mkdir data
 curl -o data/trees.json 'https://opendata.camden.gov.uk/resource/2ujt-4csu.json?$limit=20000'
 protoc --go_out=src/trees proto/vector_tile.proto
